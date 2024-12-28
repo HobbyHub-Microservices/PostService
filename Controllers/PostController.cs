2535abe3e173646b0b7b18cb84344ee9064a9b15
JANINE <478518@student.fontys.nl>
2024-12-28 19:21:23 +0100

    changed to dotnet 8, added automapper stuff

---
+++ Controllers/PostController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PostService.Data;
+using PostService.DTOs;
+
+namespace PostService.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class PostController : ControllerBase
+{
+    private readonly IPostRepo _repo;
+    private readonly IMapper _mapper;
+
+    public PostController(
+        IPostRepo repo,
+        IMapper mapper
+        )
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<PostViewDto>> GetAllPosts()
+    {
+        Console.WriteLine("--> Getting posts from DB...");
+
+        var postItem = _repo.GetAllPostsAsync();
+
+        return Ok(_mapper.Map<IEnumerable<PostViewDto>>(postItem));
+    }
+    
+}
---
+++ Profiles/PostProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PostService.DTOs;
+using PostService.Models;
+
+namespace PostService.Profiles;
+
+public class PostProfile : Profile
+{
+    public PostProfile()
+    {
+        CreateMap<PostCreateDTO, Post>()
+            .ForMember(dest => dest.PostId, opt => opt.MapFrom(src => Guid.NewGuid())) // Auto-generate the PostId
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow)); // Set the created timestamp to current time
+
+        // Map from Post to PostViewDto (for returning data in viewable format)
+        CreateMap<Post, PostViewDto>()
+            .ForMember(dest => dest.HobbyName, opt => opt.MapFrom(src => "HOBBY_NAME_PLACEHOLDER")) // Placeholder, assuming you'll fetch this data from another service
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => "USER_NAME_PLACEHOLDER")); // Placeholder, assuming you'll fetch this data from another service
+    }
+}
